ab7bd2437d0a9b133ca05a65007f03cfc36dece6
=Canh Vu <=kanhknight@gmail.com>
2018-05-29 02:05:22 +0700

    Bài Số 3

--- Session5/Homework/app.py
+++ Session5/Homework/app.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/bai1')
+def bai1():
+    return render_template('bai1.html')
+
 @app.route('/bai2')
 def bai2():
     return render_template('bai2.html')
 
-@app.route('/bai1')
-def bai1():
-    return render_template('bai1.html')
+@app.route('/bai3')
+def bai3():
+    return render_template('bai3.html')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
